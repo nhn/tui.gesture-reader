e848ef458c72d3ece0e465ee48a03d6ea8db4bb2
jein.yi <jein.yi@nhnent.com>
2015-07-24 11:58:04 +0900

    gesture reader readme

---
+++ 1.1.0/tutorials/src/js/gesture-reader.js
@@ -0,0 +1,369 @@
+(function() {
+/**
+ * @fileoverview discriminate type of touch event
+ */
+
+ne.util.defineNamespace('ne.component.Gesture');
+
+/**
+ * To find out it's flick or click or nothing from event datas.
+ * @namespace ne.component.Gesture.Reader
+ * @example
+ * var reader = new ne.component.Gesture.Reader({
+ *      type : 'flick' || 'longtab' || 'doubleclick'
+ * });
+ */
+ne.component.Gesture.Reader = ne.util.defineClass(/** @lends ne.component.Gesture.Reader.prototype */{
+    /**
+     * set options
+     * @param {object} option
+     *      @param {number} [option.flickTime] time to check flick
+     *      @param {number} [option.flickRange] The range to check flick
+     *      @param {number} [option.clickTime] The time to check click
+     *      @param {number} [option.minDist] Distance to check movement for longtab and flick
+     *      @param {number} [option.maxDist] Distance to check movement for doubleclick
+     */
+    init: function(option) {
+        if (option.type === 'flick') {
+            ne.util.extend(this, ne.component.Gesture.Reader.Flick);
+        } else if (option.type === 'longtab') {
+            ne.util.extend(this, ne.component.Gesture.Reader.LongTab);
+        } else if (option.type === 'dbclick') {
+            ne.util.extend(this, ne.component.Gesture.Reader.DoubleClick);
+        }
+        this.initialize(option);
+    }
+});
+/**
+ * @fileoverview discriminate doubleclick event
+ */
+
+ne.util.defineNamespace('ne.component.Gesture.Reader.DoubleClick');
+
+ne.component.Gesture.Reader.DoubleClick = /** @lends ne.component.Gesture.Reader.DoubleClick */{
+    /**
+     * Timer for check click twice in time
+     */
+    clickTimer: null,
+    /**
+     * The type of reader
+     */
+    type: 'dbclick',
+    /**
+     * Maximum safe distance
+     */
+    maxDist: 10,
+    /**
+     * Available double click term
+     */
+    clickTerm: 200,
+    /**
+     * First click timestamp
+     */
+    startTime: null,
+
+    /**
+     * Initailize
+     * @param option
+     */
+    initialize: function(option) {
+        this.clickTerm = option.clickTerm || this.clickTerm;
+        this.maxDist = option.maxDist || this.maxDist;
+    },
+
+    /**
+     * Check click or double click
+     * @param {number} timeDist distance from mousedown/touchstart to mouseup/touchend
+     * @private
+     * @returns {*}
+     * @example
+     * reader.isDoubleClick({
+     *      x: 10,
+     *      y: 10
+     * });
+     */
+    isDoubleClick: function(pos) {
+        var time = new Date(),
+            start = this.startTime,
+            isDoubleClick;
+        if (start && this.isAvailableZone(pos)) {
+            this.clearTimer();
+            this.startTime = null;
+            isDoubleClick = true;
+        } else {
+            this.setTimer();
+            this.pos = pos;
+            this.startTime = time;
+            isDoubleClick = false;
+        }
+        return isDoubleClick;
+    },
+
+    /**
+     * Compare with saved position to safe zone
+     * @param {object} pos Position to compare with saved position
+     */
+    isAvailableZone: function(pos) {
+        var isAvailX = Math.abs(this.pos.x - pos.x) < this.maxDist,
+            isAvailY = Math.abs(this.pos.y - pos.y) < this.maxDist;
+
+        return isAvailX && isAvailY;
+    },
+
+    /**
+     * Set timer to check click term
+     */
+    setTimer: function() {
+        this.clickTimer = window.setTimeout(ne.util.bind(function() {
+            this.startTime = null;
+        }, this), this.clickTerm);
+    },
+
+    /**
+     * Clear timer
+     */
+    clearTimer: function() {
+        window.clearTimeout(this.clickTimer);
+    }
+};
+/**
+ * @fileoverview discriminate flick event
+ */
+
+ne.util.defineNamespace('ne.component.Gesture.Reader.Flick');
+
+ne.component.Gesture.Reader.Flick = /** @lends ne.component.Gesture.Reader.Flick */{
+    /**
+     * time is considered flick.
+     */
+    flickTime: 100,
+    /**
+     * width is considered flick.
+     */
+    flickRange: 300,
+    /**
+     * width is considered moving.
+     */
+    minDist: 10,
+    /**
+     * Reader type
+     */
+    type: 'flick',
+
+    /**
+     * Initialize Flicking
+     */
+    initialize: function(option) {
+        this.flickTime = option.flickTime || this.flickTime;
+        this.flickRange = option.flickRange || this.flickRange;
+        this.minDist = option.minDist || this.minDist;
+    },
+
+    /**
+     * pick event type from eventData
+     * @param {object} eventData event Data
+     * @return {object}
+     */
+    figure: function(eventData) {
+        return {
+            direction : this.getDirection(eventData.list),
+            isFlick: this.isFlick(eventData)
+        }
+    },
+
+    /**
+     * return direction figured out
+     * @param {array} list eventPoint List
+     * @returns {string}
+     */
+    getDirection: function(list) {
+        var first = list[0],
+            final = list[list.length-1],
+            cardinalPoint = this.getCardinalPoints(first, final),
+            res = this.getCloseCardinal(first, final, cardinalPoint);
+
+        return res;
+    },
+    /**
+     * return cardinal points figured out
+     * @param {object} first start point
+     * @param {object} last end point
+     */
+    getCardinalPoints: function(first, last) {
+        var verticalDist = first.y - last.y,
+            horizonDist = first.x - last.x,
+            NS = '',
+            WE = '';
+
+        if (verticalDist < 0) {
+            NS = 'S';
+        } else if (verticalDist > 0) {
+            NS = 'N';
+        }
+
+        if (horizonDist < 0) {
+            WE = 'E';
+        } else if (horizonDist > 0) {
+            WE = 'W';
+        }
+
+        return NS+WE;
+    },
+
+    /**
+     * return nearest four cardinal points
+     * @param {object} first start point
+     * @param {object} last end point
+     * @param {string} cardinalPoint cardinalPoint from getCardinalPoints
+     * @returns {string}
+     */
+    getCloseCardinal: function(first, last, cardinalPoint) {
+        var slop = (last.y - first.y) / (last.x - first.x),
+            direction;
+        if (slop < 0) {
+            direction = slop < -1 ? 'NS' : 'WE';
+        } else {
+            direction = slop > 1 ? 'NS' : 'WE';
+        }
+
+        direction = ne.util.getDuplicatedChar(direction, cardinalPoint);
+        return direction;
+    },
+
+    /**
+     * extract type of event
+     * @param {object} eventData event data
+     * @returns {string}
+     * @example
+     * reader.isFlick({
+     *      start: 1000,
+     *      end: 1100,
+     *      list: [
+     *            {
+     *                x: 10,
+     *                y: 10
+     *            },
+     *            {
+     *                x: 11,
+     *                y: 11
+     *            }
+     *      ]
+     * });
+     */
+    isFlick: function(eventData) {
+        var start = eventData.start,
+            end = eventData.end,
+            list = eventData.list,
+            first = list[0],
+            final = list[list.length - 1],
+            timeDist = end - start,
+            xDist = Math.abs(first.x - final.x),
+            yDist = Math.abs(first.y - final.y),
+            isFlick;
+
+        if (timeDist < this.flickTime || xDist > this.flickRange || yDist > this.flickRange) {
+            isFlick = true;
+        } else {
+            isFlick = false;
+        }
+
+        return isFlick;
+    }
+};
+/**
+ * @fileoverview discriminate longtab event
+ */
+
+ne.util.defineNamespace('ne.component.Gesture.Reader.LongTab');
+
+ne.component.Gesture.Reader.LongTab = /** @lends ne.component.Gesture.Reader.LongTab */{
+    /**
+     * width is considered moving.
+     */
+    minDist: 10,
+    /**
+     * tab timer for check double click
+     */
+    tabTimer: null,
+    /**
+     * extracted event type
+     */
+    type: 'longtab',
+    /**
+     * long tab term
+     */
+    longTabTerm: 600,
+    /**
+     * set options
+     * @param {object} option
+     *      @param {number} [option.flickTime] time to check flick
+     *      @param {number} [option.flickRange] range to check flick
+     *      @param {number} [option.clickTime] time to check click
+     *      @param {number} [option.minDist] distance to check movement
+     */
+    initialize: function(option) {
+        this.minDist = option.flickTime || this.minDist;
+        this.longTabTerm = option.longTabTerm || this.longTabTerm;
+    },
+
+    /**
+     * Start detect longtab roop, If touchstop event does not fire and position are same, run callback
+     * @param {object} pos position to start
+     * @param {function} callback
+     */
+    startTab: function(pos) {
+        this.isLongtabed = false;
+        this.longTabPos = pos;
+        this.tabTimer = window.setTimeout(ne.util.bind(function() {
+            this.isLongtabed = true;
+        }, this), this.longTabTerm);
+    },
+
+    /**
+     * Stop detect longtab roop.
+     * @param {object} pos position to end
+     * @param {function} callback
+     * @return
+     * @example
+     * reader.isLongTab({
+     *      x: 10,
+     *      y: 10
+     * }, function() {
+     *      showMenu();
+     * });
+     */
+    isLongTab: function(pos, callback) {
+        var isSafeX,
+            isSafeY,
+            isLongtab = false;
+        if (this.isLongtabed) {
+            isSafeX = Math.abs(this.longTabPos.x - pos.x) < this.minDist;
+            isSafeY = Math.abs(this.longTabPos.y - pos.y) < this.minDist;
+            if (isSafeX && isSafeY) {
+                isLongtab = true;
+                if (callback) {
+                    callback();
+                }
+                this.stopTab();
+            }
+        }
+        return isLongtab;
+    },
+
+    /**
+     * Stop long tab check
+     */
+    stopTab: function() {
+        this.isLongtabed = false;
+        this.resetTimer();
+    },
+
+    /**
+     * clear clickTimer
+     */
+    resetTimer: function() {
+        window.clearTimeout(this.tabTimer);
+        this.tabTimer = null;
+    }
+};
+})();
--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 Gesture Reader
 ===============
-제스처 리더 컴포넌트<br>이벤트 또는 포인트 좌표로 이벤트의 타입과 종류를 판별한다.
+Gesture Reader Component.<br>
+Distinguish gesture with event object and position points.
 
 ## Feature
-* 각 사용 이벤트별 reader 를 생성할 수 있다.
-* Flick : 터치이벤트 좌표들을 통해 이동방향및 플릭 여부를 추적한다.
+* Create reader to distinguish each event.
+* Flick : Distinguish flick or not.
 	* N/E/S/W
 	* NE/ES/SW/WN
 	* isFlick
-* LongTab : 롱탭인지 아닌지 여부를 판단한다.
-* DoubleClick : 더블클릭 여부를 판별한다.
+* LongTab : Distinguish logTab or not.
+* DoubleClick : Distinguish double click or not.
 
 ## Documentation
 * **API** : https://nhnent.github.io/fe.component-gesture-reader/lastest
-* **Tutorial** : https://github.com/nhnent/fe.component-gesture-reader/wiki/제스처-리더-컴포넌트-적용방법
+* **Tutorial** : https://github.com/nhnent/fe.component-gesture-reader/wiki/Gesture-Reader-Tutorial
 * **Sample** - https://nhnent.github.io/fe.component-gesture-reader/lastest/tutorial-sample1.html
-
 
 
 
@@ -36,15 +36,15 @@ Gesture Reader
 
 ## Download/Install
 * Bower:
-   * 최신버전 : `bower install ne-component-gesture-reader#master`
-   * 특정버전 : `bower install ne-component-gesture-reader[#tag]`
+   * lastest : `bower install ne-component-gesture-reader#master`
+   * each version : `bower install ne-component-gesture-reader[#tag]`
 * Download: https://github.com/nhnent/fe.component-gesture-reader
 
 ## History
 | Version | Description | Date | Developer |
 | ---- | ---- | ---- | ---- |
-| <a href="https://github.nhnent.com/pages/fe/component-gesture-reader/1.1.0">1.1.0</a> | 리팩토링 및 모듈 분리, API변경 | 2015.05 | FE개발팀 이제인 <jein.yi@nhnent.com> |
-| <a href="https://github.nhnent.com/pages/fe/component-gesture-reader/1.0.0">1.0.0</a> | 배포 | 2015.05 | FE개발팀 이제인 <jein.yi@nhnent.com> |
+| <a href="https://github.nhnent.com/pages/fe/component-gesture-reader/1.1.0">1.1.0</a> | Refactoring and change structure, API changed | 2015.05 |NHN ent. FE Dev team. Jein Yi<jein.yi@nhnent.com> |
+| <a href="https://github.nhnent.com/pages/fe/component-gesture-reader/1.0.0">1.0.0</a> | Release | 2015.05 | NHN ent. FE Dev team. Jein Yi <jein.yi@nhnent.com> |
 
 ## LICENSE
-[MIT 라이선스](LICENSE)로 자유롭게 사용할 수 있습니다.
+[MIT LICENSE](LICENSE)
